f73ec7811d95c30e5dc833e7a01350297463018a
MashifaneNeo <ST10449143@imconnect.edu.za>
2025-08-23 20:56:46 +0200

    Add project files.

---
+++ ST10449143_CLDV6212_POEPART1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ST10449143_CLDV6212_POEPART1.Models;
+
+namespace ST10449143_CLDV6212_POEPART1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ST10449143_CLDV6212_POEPART1/Models/Customer.cs
@@ -0,0 +1,6 @@
+﻿namespace ST10449143_CLDV6212_POEPART1.Models
+{
+    public class Customer
+    {
+    }
+}
