ce87075cdbac0c0e1f5a55cf84a7d3df6b4b7780
MashifaneNeo <ST10449143@imconnect.edu.za>
2025-08-23 23:20:15 +0200

    created controllers

---
+++ ST10449143_CLDV6212_POEPART1/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ST10449143_CLDV6212_POEPART1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
