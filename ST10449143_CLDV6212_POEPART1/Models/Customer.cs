f43b4fa5f981a0d887cbe88d3bb1f58626fb5b3f
MashifaneNeo <ST10449143@imconnect.edu.za>
2025-08-24 23:09:41 +0200

    Fixed the edit button for customer

--- ST10449143_CLDV6212_POEPART1/Models/Customer.cs
+++ ST10449143_CLDV6212_POEPART1/Models/Customer.cs
@@ -2,6 +2,7 @@
 using Azure;
 using Azure.Data.Tables;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace ST10449143_CLDV6212_POEPART1.Models
 {
@@ -13,7 +14,15 @@ namespace ST10449143_CLDV6212_POEPART1.Models
 
         public DateTimeOffset? Timestamp { get; set; }
 
+        [IgnoreDataMember] // prevents direct serialization
         public ETag ETag { get; set; }
+
+        [Display(Name = "ETag")]
+        public string ETagValue
+        {
+            get => ETag.ToString();
+            set => ETag = string.IsNullOrEmpty(value) ? default : new ETag(value);
+        }
 
         [Display(Name = "Customer ID")]
         public string CustomerId => RowKey;
